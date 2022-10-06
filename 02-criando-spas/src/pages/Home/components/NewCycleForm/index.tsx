88d73e305208fc3f7c3e9d923186d57a32f2907d
felipesses <felipeme73@gmail.com>
2022-10-06 19:07:23 -0300

    feat: finish second project

---
+++ 02-criando-spas/src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,44 @@
+import { FormContainer, TaskInput, MinutesAmountInput } from './styles'
+import { useContext } from 'react'
+import { useFormContext } from 'react-hook-form'
+import { CyclesContext } from '../../../../contexts/CyclesContext'
+
+export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext)
+
+  const { register } = useFormContext()
+
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        type="text"
+        placeholder="Dê um nome para o seu projeto"
+        list="task-suggestions"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+
+      <datalist id="task-suggestions">
+        <option value="Projeto 1" />
+        <option value="Projeto 2" />
+        <option value="Projeto 3" />
+        <option value="A" />
+      </datalist>
+
+      <label htmlFor="minutesAmount">durante</label>
+      <MinutesAmountInput
+        id="minutesAmount"
+        type="number"
+        placeholder="00"
+        disabled={!!activeCycle}
+        min={5}
+        max={60}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
